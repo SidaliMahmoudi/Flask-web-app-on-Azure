ca272aa8894fa2d8a0bcd363f11b17e3535e82d3
Sid Ali MAHMOUDI <fs_mahmoudi@esi.dz>
2019-05-14 22:24:32 +0100

    Modifying the main code

--- flask-web-app.py
+++ flask-web-app.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Testing deployement flask app on azure"
+
+if __name__ == '__main__':
+  app.secret_key = 'super_secret_key'
+  app.debug = True
+  app.run(host = '0.0.0.0', port = 5000)
